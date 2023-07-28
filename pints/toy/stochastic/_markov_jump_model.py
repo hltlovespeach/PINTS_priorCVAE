98af86f7f7e74ad34aa99182ead71377fec1bc07
LutongHao <119966228+hltlovespeach@users.noreply.github.com>
2023-07-28 13:36:24 +0100

    jax compilation

--- pints/toy/stochastic/_markov_jump_model.py
+++ pints/toy/stochastic/_markov_jump_model.py
@@ -6,9 +6,16 @@
 # copyright notice and full license details.
 #
 import numpy as np
-import scipy.interpolate
+import scipy.interpolate as interpolate
 
 import pints
+
+import jax
+import jax.numpy as jnp
+import jax.scipy
+from jax_cosmo import scipy as jcospy
+
+import random as rnd
 
 from .. import ToyModel
 
@@ -77,11 +84,16 @@ class MarkovJumpModel(pints.ForwardModel, ToyModel):
     """
     def __init__(self, x0, V, propensities):
         super(MarkovJumpModel, self).__init__()
-        self._x0 = np.asarray(x0)
+        self._x0 = jnp.asarray(x0)
         self._V = V
         self._propensities = propensities
-        if any(self._x0 < 0):
-            raise ValueError('Initial molecule count cannot be negative.')
+
+        t = 0
+        x = jnp.array(self._x0)
+        self.mol_count = [jnp.array(x)]
+        self.time = [t]
+        # if jnp.any(self._x0 < 0):
+        #     raise ValueError('Initial molecule count cannot be negative.')
 
     def n_parameters(self):
         """ See :meth:`pints.ForwardModel.n_parameters()`. """
@@ -89,88 +101,236 @@ class MarkovJumpModel(pints.ForwardModel, ToyModel):
 
     def simulate_raw(self, rates, max_time):
         """ Returns raw times, mol counts when reactions occur. """
-        if len(rates) != self.n_parameters():
-            raise ValueError(
-                'This model should have ' + str(self.n_parameters())
-                + ' parameter(s).')
+        # if len(rates) != self.n_parameters():
+        #     raise ValueError(
+        #         'This model should have ' + str(self.n_parameters())
+        #         + ' parameter(s).')
 
         # Setting the current propensities and summing them up
         current_propensities = self._propensities(self._x0, rates)
-        prop_sum = np.sum(current_propensities)
+        cp = jnp.asarray(current_propensities)
+        prop_sum = jnp.sum(cp)
 
         # Initial time and count
         t = 0
-        x = np.array(self._x0)
+        # x = jnp.array(self._x0)
 
         # Run Gillespie SSA, calculating time until next reaction, deciding
         # which reaction, and applying it
-        mol_count = [np.array(x)]
-        time = [t]
-        while prop_sum > 0 and t <= max_time:
-            r_1, r_2 = np.random.uniform(0, 1), np.random.uniform(0, 1)
-            t += -np.log(r_1) / prop_sum
-            s = 0
-            r = 0
-            while s <= r_2 * prop_sum:
-                s += current_propensities[r]
-                r += 1
-            x += self._V[r - 1]
+        # self.mol_count = [jnp.array(x)]
+        # self.time = [t]
+        
+        arr = [prop_sum, t, max_time, cp]
+        def cond(arr):
+            return jnp.logical_and(arr[0] > 0, arr[1] <= arr[2])
+        
+        def body(arr):
+            
+            cp_1 = jnp.asarray(self._propensities(self._x0, rates))
+            x = jnp.array(self._x0)
+            V = jnp.asarray(self._V)
+
+            # time = self.time
+            # mol_count = self.mol_count
+
+            key_1, key_2 = jax.random.split(jax.random.PRNGKey(rnd.randint(0, 9999)))
+            r_1, r_2 = jax.random.uniform(key_1), jax.random.uniform(key_2)
+            arr[1] += -jnp.log(r_1) / arr[0]
+
+            def cond_1(sr):
+                return sr[0][0] <= r_2 * arr[0]
+            
+            def body_2(sr):
+                idx = sr[1][0]
+                ad = cp_1[idx]
+                sr = sr.at[1].add(1)
+                sr = sr.at[0].add(ad)
+                return jnp.asarray(sr)
+
+            srzero = jnp.array([[0],[0]])
+            newsr = jax.lax.while_loop(cond_fun=cond_1, body_fun=body_2, init_val=srzero)
+
+            x += V[newsr[1][0] - 1]
 
             # Calculate new current propensities
             current_propensities = self._propensities(x, rates)
-            prop_sum = np.sum(current_propensities)
+            current_propensities = jnp.asarray(current_propensities)
+            arr[0] = jnp.sum(current_propensities)
 
             # Store new values
-            time.append(t)
-            mol_count.append(np.copy(x))
+            self.time.append(arr[1])
+            self.mol_count.append(x)
+            
+            return arr
 
-        return np.array(time), np.array(mol_count)
+        last = jax.lax.while_loop(cond_fun=cond, body_fun=body, init_val=arr)
 
-    def interpolate_mol_counts(self, time, mol_count, output_times):
+        return self.time, self.mol_count
+
+    def sorted_interp(self, x, xp, fp):
+        m = x.shape[0]
+        n = xp.shape[0]
+        x = jnp.atleast_1d(x)
+        j = 0
+        xp_0 = xp[0]
+        fp_0 = fp[0]
+
+        def inner_fun(args):
+            x_i, j = args
+            def cond_fun(state):
+                is_continuing, *_ = state
+                return is_continuing
+
+            def body_fun(state):
+                _, _, curr_j, curr_xp_j, curr_fp_j = state
+
+                next_xp_j = xp[curr_j + 1]
+                next_fp_j = fp[curr_j + 1]
+
+                cond = x_i > next_xp_j
+
+                def cond_true(_):
+                    inner_cond = curr_j + 1 == n - 1
+
+                    def fun_true(_): return False, True, curr_j, next_xp_j, next_fp_j
+                    def fun_false(_): return True, False, curr_j + 1, next_xp_j, next_fp_j
+
+                    return lax.cond(inner_cond, fun_true, fun_false, None)
+
+                def cond_false(_):
+                    inner_cond = curr_fp_j == next_xp_j
+                
+                    def fun_true(_):  return False, True, curr_j, next_xp_j, next_fp_j
+                    def fun_false(_):  return False, False, curr_j, next_xp_j, next_fp_j
+
+                    return lax.cond(inner_cond, fun_true, fun_false, None)
+
+                return lax.cond(cond, cond_true, cond_false, None)
+
+            _, use_next_fp_j, new_j, *_ = lax.while_loop(cond_fun, body_fun, (True, False, j, xp[j], fp[j]))
+            # We don't compute the result inside the loop to allow for seemless backward mode differentiability
+            return new_j, lax.cond(use_next_fp_j, 
+                                    lambda _: fp[new_j + 1],
+                                   lambda _: fp[new_j] + + (fp[new_j + 1] - fp[new_j]) * (x_i - xp[new_j]) / (xp[new_j + 1] - xp[new_j]),
+                                   None)
+
+        def body_fun(j, x_i):
+            return lax.cond(x_i <= xp_0, lambda *_: (j, fp_0), inner_fun, (x_i, j))
+
+        _, f = lax.scan(body_fun, 0, x)
+        return f
+        
+    # def interp(self, x, xp, fp, left=None, right=None, period=None):
+    #     x, xp, fp = map(jnp.asarray, (x, xp, fp))
+    #     if period:
+    #         x = x % period
+    #         xp = xp % period
+    #         i = jnp.argsort(xp)
+    #         xp = xp[i]
+    #         fp = fp[i]
+    #         xp = jnp.concatenate([xp[-1:] - period, xp, xp[:1] + period])
+    #         fp = jnp.concatenate([fp[-1:], fp, fp[1:]])
+  
+    #     i = jnp.clip(jnp.searchsorted(xp, x, side='right'), 1, len(xp) - 1)
+    #     f = (fp[i - 1] *  (xp[i] - x) + fp[i] * (x - xp[i - 1])) / (xp[i] - xp[i - 1])
+
+    #     if not period:
+    #         if left is None:
+    #             left = fp[0]
+    #         if right is None:
+    #             right = fp[-1]
+    #         f = jnp.where(x < xp[0], left, f)
+    #         f = jnp.where(x > xp[-1], right, f)
+    #     return f
+
+    def interpolate_mol_counts(self, time_interp, mol_count, output_times):
         """
         Takes raw times and inputs and mol counts and outputs interpolated
         values at output_times
         """
-        if len(time) == 0:
+        if len(time_interp) == 0:
             raise ValueError('At least one time must be given.')
-        if len(time) != len(mol_count):
-            raise ValueError(
-                'The number of entries in time must match mol_count')
+        if len(time_interp) != len(mol_count):
+            raise ValueError('The number of entries in time must match mol_count')
 
         # Check output times
-        output_times = np.asarray(output_times)
-        if not np.all(output_times[1:] >= output_times[:-1]):
-            raise ValueError('The output_times must be non-decreasing.')
+        output_times = jnp.asarray(output_times)
+        # if jnp.logical_not(jnp.all(output_times[1:] >= output_times[:-1])) :
+        #     raise ValueError('The output_times must be non-decreasing.')
 
         # Interpolate as step function, decreasing mol_count by 1 at each
         # reaction time point.
-        if len(time) == 1:
+        if len(time_interp) == 1:
             # Need at least 2 values to interpolate
-            return np.ones(len(output_times)) * mol_count[0]
+            return jnp.ones(len(output_times)) * mol_count[0]
         else:
             # Note: Can't use fill_value='extrapolate' here as:
             #  1. This require scipy >= 0.17
             #  2. There seems to be a bug in some scipy versions
-            interp_func = scipy.interpolate.interp1d(
-                time, mol_count, kind='previous', axis=0, fill_value=np.nan,
-                bounds_error=False)
-            values = interp_func(output_times)
 
+            values = self.sorted_interp(output_times, time_interp, mol_count)
+            # jcospy.interpolate.
+            # values = jax.numpy.interp(output_times, time_interp, mol_count)
+            # interp_function = jax.scipy.interpolate.RegularGridInterpolator(
+                # time_interp, mol_count,fill_value=jnp.nan, bounds_error=False)
+            # values = interp_func(output_times)
+        
         # At any point past the final time, repeat the last value
-        values[output_times >= time[-1]] = mol_count[-1]
-        values[output_times < time[0]] = mol_count[0]
+        values[output_times >= time_interp[-1]] = mol_count[-1]
+        values[output_times < time_interp[0]] = mol_count[0]
 
         return values
 
     def simulate(self, parameters, times):
         """ See :meth:`pints.ForwardModel.simulate()`. """
-        times = np.asarray(times)
-        if np.any(times < 0):
-            raise ValueError('Negative times are not allowed.')
+        times = jnp.asarray(times)
+        # if jnp.any(times < 0):
+        #     raise ValueError('Negative times are not allowed.')
 
         # Run Gillespie algorithm
-        time, mol_count = self.simulate_raw(parameters, max(times))
+        time, mol_count = self.simulate_raw(parameters, jnp.max(times))
+
+        print(time)
+        # print(mol_count)
 
         # Interpolate and return
         return self.interpolate_mol_counts(time, mol_count, times)
 
+
+
+
+
+# original while loop in simulate_raw
+ # while (prop_sum > 0) and (t <= max_time):
+        #     r_1, r_2 = jnp.random.uniform(0, 1), jnp.random.uniform(0, 1)
+        #     t += -jnp.log(r_1) / prop_sum
+        #     s = 0
+        #     r = 0
+        #     while s <= r_2 * prop_sum:
+        #         s += current_propensities[r]
+        #         r += 1
+        #     x += self._V[r - 1]
+
+        #     # Calculate new current propensities
+        #     current_propensities = self._propensities(x, rates)
+        #     current_propensities = jnp.asarray(current_propensities)
+        #     prop_sum = jnp.sum(current_propensities)
+
+        #     # Store new values
+        #     time.append(t)
+        #     mol_count.append(jnp.copy(x))
+
+# cond_1 = (sr[0] <= r_2 * prop_sum)
+# def true_fun_1(sr):
+#     sr[0] += current_propensities[sr[1]]
+#     sr[1] += 1
+
+# def false_fun_1(sr):
+#     return sr
+
+# f_1 = jax.lax.cond(pred=cond_1, true_fun=true_fun_1, false_fun=false_fun_1, operands=sr)
+
+# jax.lax.scan(f_1,)
+# def add_index(s,r):
+
+# jax.lax.scan(f, init, xs, length)
